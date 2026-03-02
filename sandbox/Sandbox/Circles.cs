c55fbf1a6b7ac59bfd54ae48e66a8fb817a54c18
douglaslaursen <douglaslaursen@gmail.com>
2026-03-02 10:02:10 -0700

    Class sandbox things! :)

---
+++ sandbox/Sandbox/Circles.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.CompilerServices;
+
+class Circle
+{
+    public double _radius; // Only attrubie in this class
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public void SetRadius(double radius)  //set radius
+    {
+        _radius = radius;
+    }
+}
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,26 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
-    // ---------------------------- functions!!!!!! -----------------------------------------------
-    public static void greeting() 
-    {
-        Console.WriteLine("Hello Bob");
-    }
+    // // ---------------------------- functions!!!!!! -----------------------------------------------
+    // public static void greeting() 
+    // {
+    //     Console.WriteLine("Hello Bob");
+    // }
 
-    public static int AddNumbers(int n1, int n2) // int is showing I am returning something! Retrun ends the statement! 
-    {
-        Console.WriteLine("Bob");
-        return n1 + n2;
-    }
+    // public static int AddNumbers(int n1, int n2) // int is showing I am returning something! Retrun ends the statement! 
+    // {
+    //     Console.WriteLine("Bob");
+    //     return n1 + n2;
+    // }
 
     static void Main(string[] args)
     {
-        // Calling the function
-        greeting(); 
 
-        int total = AddNumbers(10, 20);
-        Console.WriteLine(total);
+        // int x = 20;
+        Console.WriteLine("Circle");
+
+        Circle myCircle = new Circle();
+
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetRadius());
+
+
+
+        Console.WriteLine("Circle 2");
+
+        Circle myCircle2 = new Circle();
+
+        myCircle2.SetRadius(99999999);
+        Console.WriteLine(myCircle2.GetRadius());
+
+
+    //     // Calling the function
+    //     greeting(); 
+
+    //     int total = AddNumbers(10, 20);
+    //     Console.WriteLine(total);
         // -------------------------------------------------- For loops -----------------------------------------------------------------
         // for (int i = 0; i < 10; i += 3) // i++ to make it just add 1
         // {
@@ -99,4 +119,35 @@ class Program
         // }
 
     }
+    // public static void passByValue(int a)
+    //     {
+    //         a++;
+    //         Console.WriteLine($"In the value function: {a}");
+    //     }
+
+    // public static void passByRef(ref int x)
+    // {
+    //     x++;
+    //     Console.WriteLine($"In the ref function: {x}");
+    // }
+
+    // public static void passByOut(out int y)  // Input from the user
+    // {
+    //     y = 10;
+    // }
+
+    // static void Main(string[] args)
+    //     {
+    //         int x = 20;
+    //         Console.WriteLine($"In the main 1: {x}");
+
+    //         passByValue(x);
+    //         Console.WriteLine($"In the main 2: {x}");
+    //         passByRef(ref x);
+    //         Console.WriteLine($"In the main 3: {x}");
+
+    //         int z;
+    //         passByOut(out z);  //Data types must match! 
+    //         Console.WriteLine(z);
+    // }
 }
