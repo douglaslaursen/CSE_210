04ba00fee0daa38cf8ffab18b138df2c94af17ce
douglaslaursen <douglaslaursen@gmail.com>
2026-03-10 09:58:05 -0600

    Class stuff  Angle

---
+++ sandbox/Angle/Angle.cs
@@ -0,0 +1,45 @@
+
+class Angle
+{
+    private double _radians;
+
+    public Angle()
+    {
+        _radians = 0.0;
+    }
+
+    public Angle(double radians)
+    {
+        _radians = radians;
+    }
+
+    public Angle(int degrees)
+    {
+        _radians = ToRadians(degrees);
+    }
+
+    public double GetRadians()
+    {
+        return _radians;
+    }
+
+    public double GetDegrees()
+    {
+        return ToDegrees();
+    }
+
+    public void SetDegrees(double degrees)
+    {
+        _radians = ToRadians(degrees);
+    }
+
+    private double ToRadians(double degrees)
+    {
+        return degrees / 180 * Math.PI;
+    }
+
+    private double ToDegrees()
+    {
+        return _radians / Math.PI * 180;
+    }
+}
--- sandbox/Angle/Program.cs
+++ sandbox/Angle/Program.cs
@@ -2,6 +2,19 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hey Bob");
+        // Making the program clean looking
+        Console.Clear();
+
+        Angle myAngle = new Angle(180);
+
+        Console.WriteLine(myAngle.GetRadians());
+
+        Console.WriteLine(myAngle.GetDegrees());
+
+        myAngle.SetDegrees(270);
+
+        Console.WriteLine(myAngle.GetRadians());
+
+        // myAngle.ToRadians(); // Does not work because that method is private. 
     }
 }
