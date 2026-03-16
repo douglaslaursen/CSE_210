4e4f99dfe38fab6357ce7dcb5dedc16e62aad5f5
douglaslaursen <douglaslaursen@gmail.com>
2026-03-16 12:10:27 -0600

    What we did in class with learning about inheritance

---
+++ sandbox/PersonProject/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        Person person = new Person("Bob", "Ross", 55, 195);
+
+        Console.WriteLine(person.GetPersonInformation());
+
+        Doctor myDoctor = new Doctor("Ax", "Bob", "Budge", 72, 210);
+        Console.WriteLine(myDoctor.GetDoctorInformation());
+    }
+}
