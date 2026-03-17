1efd80634fbcce7b9ddbdf3a8939527c33e6b29b
douglaslaursen <douglaslaursen@gmail.com>
2026-03-17 09:40:26 -0600

    Added police and edited person and program

--- sandbox/PersonProject/Program.cs
+++ sandbox/PersonProject/Program.cs
@@ -1,13 +1,51 @@
 ﻿class Program
 {
+    public static void ProcessPerson(Person person)
+    {
+        if (person is Police police)
+        {
+            Console.WriteLine(police.GetPoliceInformation());
+        }
+        else if (person is Doctor doctor)
+        {
+            Console.WriteLine(doctor.GetDoctorInformation());
+        }
+        else
+        {
+            Console.WriteLine(person.GetPersonInformation());
+        }
+    }
     public static void Main(string[] args)
     {
         Console.Clear();
+        Console.WriteLine();
         Person person = new Person("Bob", "Ross", 55, 195);
-
         Console.WriteLine(person.GetPersonInformation());
 
+        Console.WriteLine();
         Doctor myDoctor = new Doctor("Ax", "Bob", "Budge", 72, 210);
         Console.WriteLine(myDoctor.GetDoctorInformation());
+
+        Console.WriteLine();
+        Police myPolice = new Police("Gun", "Lance", "Bob", 43, 198);
+        Console.WriteLine(myPolice.GetPoliceInformation());
+
+        Console.WriteLine();
+        myPolice.SetWeight(myPolice.GetWeight() + 5);
+        myPolice.SetAge(myPolice.GetAge() + 1);
+        Console.WriteLine(myPolice.GetPoliceInformation());
+        Console.WriteLine();
+
+
+        List<Person> myPeople = new List<Person>();
+        myPeople.Add(person);
+        myPeople.Add(myDoctor);
+        myPeople.Add(myPolice);
+
+        foreach(Person tempPerson in myPeople)
+        {
+            // Console.WriteLine(tempPerson.GetPersonInformation());
+            ProcessPerson(tempPerson);
+        }
     }
 }
--- sandbox/PersonProject/person.cs
+++ sandbox/PersonProject/person.cs
@@ -12,12 +12,48 @@ class Person
     {
         _firstName = firstName;
         _lastName = lastName;
-        _age = age;
-        _weight = weight;
+        SetAge(age);
+        SetWeight(weight);
+        // _age = age;
+        // _weight = weight;
     }
 
     public string GetPersonInformation()
     {
         return $"Name: {_firstName} {_lastName}, age: {_age}, weight: {_weight}";
+    }
+
+    public int GetAge()
+    {
+        return _age;
+    }
+
+    public int GetWeight()
+    {
+        return _weight;
+    }
+
+    public void SetWeight(int weight)
+    {
+        if (weight < 4 || weight > 500)
+        {
+            Console.WriteLine("Invalid Weight");
+        }
+        else
+        {
+            _weight = weight;
+        }
+    }
+
+    public void SetAge(int age)
+    {
+        if (age < 4 || age > 500)
+        {
+            Console.WriteLine("Invalid age");
+        }
+        else
+        {
+            _age = age;
+        }
     }
 }
