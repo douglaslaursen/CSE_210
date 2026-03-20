89caf570d811151f9a54a9e23383c0fe7b98f3c3
douglaslaursen <douglaslaursen@gmail.com>
2026-03-19 19:45:37 -0600

    Forgot to add the breathing activity to stage lol

---
+++ prove/Develop04/BreathingActivity.cs
@@ -0,0 +1,49 @@
+class BreathingActivity : Activity
+{
+    private int _breathInTime;
+    private int _breathOutTime;
+
+    public BreathingActivity(int breathInTime, int breathOutTime)
+    : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and fouc on your breathing. ")
+    {
+        _breathInTime = breathInTime;
+        _breathOutTime = breathOutTime;
+    }
+
+    public void RunBreathingActivity()
+    {
+        Console.Clear();
+        DisplayGreeting();
+        Console.WriteLine();
+        DisplayDescription();
+        Console.WriteLine();
+        DisplayDuration();
+        
+        Console.Clear();
+        DisplaySpinner("Get ready...", 3);
+        Console.WriteLine();
+        Console.WriteLine();
+        
+        BreathingCycle();
+
+        Console.WriteLine();
+        DisplayEnding();
+        DisplaySpinner("", 5);
+    }
+
+    private void BreathingCycle()
+    {
+        DateTime currentTime = DateTime.Now;
+        DateTime endTime = currentTime.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine();
+            RunCountDown("Breath in... ", _breathInTime);
+            Console.WriteLine();
+            RunCountDown("Breath out... ", _breathOutTime);
+            Console.WriteLine();
+
+        }
+    }
+}
