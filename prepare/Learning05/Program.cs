fc0b58507677ab50840f884ea8ce3f13bdf20061
douglaslaursen <douglaslaursen@gmail.com>
2026-03-23 11:59:36 -0600

    Finished the main and it is working.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape> ();
+
+        Square shape1 = new Square(3, "Green");
+        shapes.Add(shape1);
+
+        Rectangle shape2 = new Rectangle(6, 4, "Yellow");
+        shapes.Add(shape2);
+
+        Circle shape3 = new Circle("Blue", 5);
+        shapes.Add(shape3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has the area of {area}. ");
+        }
     }
 }
