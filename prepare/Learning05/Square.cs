32611ab56a389697d4133e7366021a1b78c44671
douglaslaursen <douglaslaursen@gmail.com>
2026-03-23 11:46:02 -0600

    Programed square

---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,15 @@
+class Square : Shape
+{
+    private double _side;
+
+    public Square(double side, string color)
+    : base (color)
+    {
+        _side = side;
+    }
+
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
